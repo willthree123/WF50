86f71c6361edefec6ccce3cddc370b6602ac52cc
willthree123 <50272419+willthree123@users.noreply.github.com>
2021-10-30 14:54:19 +0800

    Finsihed

--- config.h
+++ config.h
@@ -1,8 +1,5 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
+#pragma once
 #include "config_common.h"
-
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x7766
@@ -22,13 +19,14 @@
 #define MATRIX_COL_PINS { B6, B2, B3, B1, F7, F6 }
 #define MATRIX_ROW_PINS_RIGHT { C6, D7, E6, B4, B5 }
 #define MATRIX_COL_PINS_RIGHT { F6, F7, B1, B3, B2, B6 }
-#define UNUSED_PINS
-/* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 #define DEBOUNCE 5
 
-#define SPLIT_USB_DETECT 
+#define USE_SERIAL
+#define MASTER_LEFT
+#define SERIAL_USE_MULTI_TRANSACTION
 #define SOFT_SERIAL_PIN D2
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
 
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D0
@@ -36,17 +34,4 @@
 #define RGBLIGHT_SPLIT
 #define RGBLIGHT_ANIMATIONS
 
-
-
-#define TAPPING_TERM    200
-#define IGNORE_MOD_TAP_INTERRUPT 
-#define LOCKING_SUPPORT_ENABLE
-#define LOCKING_RESYNC_ENABLE
-#define IS_COMMAND() ( \
-    get_mods() == (MOD_BIT(KC_LCTL) | MOD_BIT(KC_RCTL)) || \
-    get_mods() == (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT)) \
-)
 #define USB_MAX_POWER_CONSUMPTION 500
-
-
-#endif
--- wf50.c
+++ wf50.c
@@ -1,1 +1,9 @@
 #include "wf50.h"
+void matrix_init_kb(void) {
+
+	DDRD &= ~(1<<5);
+	PORTD &= ~(1<<5);
+
+	DDRB &= ~(1<<0);
+	PORTB &= ~(1<<0);
+};
